269fa3c627f4008e55a5ceec4beee06270a40c0b
Haruki Oshiro <hoshiro@ivry.jp>
2025-11-07 15:50:18 +0900

    チェックコマンド

--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,9 @@ open Lake DSL
 package "LeanBook" where
   version := v!"0.1.0"
 
+--
 @[default_target]
-lean_lib «LeanBook» where
+lean_lib "LeanBook" where
+  -- NaturalNumber.leanなどを他の.
   -- add library configuration options here
+  globs := #[.submodules `LeanBook]
